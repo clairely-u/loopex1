714ee357d705466da60b2efdc9974ae7ed986dc3
Claire Utrecht <utrechtc@gmail.com>
2021-12-15 20:52:33 -0500

    commit for loop exercise 1

---
+++ script.js
@@ -0,0 +1,39 @@
+/*
+for (i = 0; i <= 10; i++) {
+    console.log(i);
+}
+for (i = 10; i >=1; i--) {
+    console.log(i);
+}
+
+
+let y = 0;
+while (y < 10) {
+    y++;
+    console.log(y)
+}
+
+let x = 10;
+while ( x > 1) {
+    x--;
+    console.log(x);
+}
+
+
+j=0;
+do {
+    j++;
+    console.log(j);
+} while (j < 10);
+
+p = 10;
+do {
+    p--;
+    console.log(p);
+} while (p > 0);
+*/
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let number of numbers) {
+    console.log(numbers);
+}
